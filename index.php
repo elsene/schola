8ea398ee6499db3d5d853aa7fde9dd2cd6770ca9
Elimane <magenelec@gmail.com>
2019-07-11 16:56:02 +0200

    scs

--- index.php
+++ index.php
@@ -185,7 +185,7 @@ function getMedecins() {
 
 
 function getActivitePatient($id_patient){
-    $sql = "SELECT * FROM effectuer_activite,patient,activite WHERE  effectuer_activite.id_patient=:id_patient and patient.id=:id_patient and activite.id=effectuer_activite.id_activite";
+    $sql = "SELECT * FROM effectuer_activite,patient,activite WHERE  effectuer_activite.id_patient=patient.id  and activite.id=effectuer_activite.id_activite";
     try{
 
        // $insert = json_decode($request->getBody());
